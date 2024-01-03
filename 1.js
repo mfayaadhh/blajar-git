e0bb50ca2363a74a38a97f28d6952c65220926da
Muhammad Fayaadh <fayaadh93@gmail.com>
2024-01-03 14:40:05 +0700

    tambah no 1

--- 1.js
+++ 1.js
@@ -1,9 +1,21 @@
 function maxMin(arrIn) {
     // Do something here
-    let x = []
-    for(let i = 0;i < arrIn.length;i++) {
-
+    if (arrIn.length <= 1) {
+        return arrIn
     }
+
+    let max = arrIn[0]
+    let min = arrIn[1]
+    for(let i = 0;i < arrIn.length;i++) {
+        if (arrIn[i] < min) {
+            min = arrIn[i]
+        }
+        for(let j = 1; j < arrIn.length; j++){
+            if(arrIn[j] > max) {
+                max = arrIn[j]
+            }
+        }
+    } return [max, min]
 }
 
 console.log(maxMin([5, 1, 7, 12, 2, 8, 5, 6, 1, 12])); // [12, 1]
